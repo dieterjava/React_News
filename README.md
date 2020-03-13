a0ced98a7c1f4bc93675fbf1a28fc6d1ad636024
dieterjava <md_hermann@yahoo.de>
2020-03-13 13:28:16 +0100

    README.md added

--- README.md
+++ README.md
@@ -2,9 +2,16 @@
 
 Created a simple React example
 
-Should read some news in json format and display them in a list with 
+Should read some news in json format from newsapi.org
+
+and display them in a list with 
 
 title
 description
 url
 image
+
+see link for displaying lists in React
+https://reactjs.org/docs/lists-and-keys.html
+
+Add source field an button with onclick 
--- index.js
+++ index.js
@@ -1,38 +1,37 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import { render } from 'react-dom';
-import Hello from './Hello';
-import './style.css';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 
+import "./style.css";
 
-  class App extends React.Component {
-
-        constructor(props){
-      super(props);
-      this.state={
-        data: '',
-        json_obj: {status: "ok", 
-        source: "time", sortBy: "top", 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: "",
+      json_obj: {
+        status: "ok",
+        source: "time",
+        sortBy: "top",
         articles: Array[30]
       }
-        }
-        }
- //  state = {}
+    };
+  }
 
-
- componentDidMount() {
- }
+  componentDidMount() { }
   componentWillMount() {
     var xhr = new XMLHttpRequest();
-    var  status = false;
-    xhr.open("GET", "https://newsapi.org/v1/articles?source=time&sortBy=latest&apiKey=1d825f6378f2460d8bcf7edc35915e0f", true);
+    var status = false;
+    xhr.open(
+      "GET",
+      "https://newsapi.org/v1/articles?source=time&sortBy=latest&apiKey=1d825f6378f2460d8bcf7edc35915e0f",
+      true
+    );
     xhr.onload = function (e) {
       if (xhr.readyState === 4) {
         if (xhr.status === 200) {
           this.state.json_obj = JSON.parse(xhr.responseText);
           console.log(this.state.json_obj);
           status = true;
-         
         } else {
           console.error(xhr.statusText);
         }
@@ -41,24 +40,21 @@ import './style.css';
     xhr.onerror = function (e) {
       console.error(xhr.statusText);
     };
-    console.log('sending');
+    console.log("sending");
     xhr.send(null);
-    console.log('sent');
+    console.log("sent");
   }
 
   render() {
-    console.log('rendering');
+    console.log("rendering");
     return (
       <div>
-      News from <a href='https://newsapi.org'> newsapi.org </a>
-      <p> status:  {this.state.json_obj.status }</p>
-      <p> source: {this.state.json_obj.source }</p>
-      <ul>{this.state.json_obj.articles} </ul>
+        News from <a href="https://newsapi.org"> newsapi.org </a>
+        <p> status: {this.state.json_obj.status}</p>
+        <p> source: {this.state.json_obj.source}</p>
+        <ul>{this.state.json_obj.articles} </ul>
       </div>
     );
   }
 }
-ReactDOM.render(
-   <App/>,
-   document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
