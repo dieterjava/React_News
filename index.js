b7c76c5e44ae4459139c0eb51886126581f22a59
dieterjava <md_hermann@yahoo.de>
2020-03-16 12:36:55 +0100

    working again

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ class App extends React.Component {
     super(props);
     this.state = {
       data: "",
-      json_objs: [ {userId: ' ', id : '1', title : 'dsfsd', body : '2312'}]
+      json_objs: [ {userId: ' ', id : '1', title : 'dsfsd', body : '2312'}],
+      news_api: [{status: "ok", source: "time", sortBy: "top", 
+      articles: Array[
+        {"author":"Lissandra Villa","title":"Co", "description":"Biden and", "url":"http://time.com/",
+        "urlToImage":"https://api.time.com/", "publishedAt":"2020-03-16T04:23:24Z" }
+      ]}
+      ]
       // {
  //       status: "ok",
  //        source: "time",
@@ -24,17 +30,18 @@ class App extends React.Component {
     var xhr = new XMLHttpRequest();
     var status = false;
     xhr.open(
-      "GET","https://jsonplaceholder.typicode.com/posts",
-   //   "https://newsapi.org/v1/articles?source=time&sortBy=latest&
-   // apiKey=1d825f6378f2460d8bcf7edc35915e0f",
+      "GET",
+      "https://jsonplaceholder.typicode.com/posts",
+    // "https://newsapi.org/v1/articles?source=time&sortBy=latest&apiKey=1d825f6378f2460d8bcf7edc35915e0f",
       true
     );
     xhr.onload = function (e) {
       if (xhr.readyState === 4) {
         if (xhr.status === 200) {
-          this.setState({json_objs : JSON.parse(xhr.responseText)});
+       this.setState({json_objs : JSON.parse(xhr.responseText)});
+        //   this.setState({news_api : JSON.parse(xhr.responseText)});
 
-        //  console.log(this.state.json_objs);
+          console.log(this.state.json_objs);
       //    this.state.jsonString = JSON.stringify(this.state.json_objs);
        //   console.log(jsonString);
           status = true;
@@ -51,11 +58,30 @@ class App extends React.Component {
     console.log("sent");
   }
 /*
-        <p> status: {this.state.json_objs.status}</p>
-        <p> source: {this.state.json_objs.source}</p>
-        <ul> {this.state.json_objs.articles}</ul>
-                News from <a href="https://newsapi.org"> newsapi.org </a> <br/>
-        Posts from <a href="https://jsonplaceholder.typicode.com/posts"> https://jsonplaceholder.typicode.com/posts</a> 
+// works
+      <ul>
+          {this.state.json_objs.map(item => (
+            <li key={item.id}>
+            Id: {item.id} <br/>
+            Title: {item.title} <br/>
+body: {item.body}
+            </li>
+          ))}
+        </ul>
+// does not work, no id  field
+articles is not an array 
+      <ul>
+          {this.state.news_api.articles.map(item => (
+            <li key={item.title}>
+            title: {item.title} <br/>
+            title: {item.title} <br/>
+description: {item.description}
+            </li>
+          ))}
+        </ul>
+
+
+
 */
 
   render() {
@@ -63,6 +89,7 @@ class App extends React.Component {
     console.log(this.state.json_objs);
     return (
       <div>
+
       <ul>
           {this.state.json_objs.map(item => (
             <li key={item.id}>
