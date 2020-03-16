7cce0b7ee05df8111a9c282d2c0acc6070c68457
dieterjava <md_hermann@yahoo.de>
2020-03-16 15:37:55 +0100

    1 file modified

--- index.js
+++ index.js
@@ -9,13 +9,15 @@ class App extends React.Component {
     this.state = {
       data: "",
       json_objs: [ {userId: ' ', id : '1', title : 'dsfsd', body : '2312'}],
-      news_api: [{status: "ok", source: "time", sortBy: "top", 
-      articles: [
-        {"author":"Lissandra Villa","title":"Co", "description":"Biden and", "url":"http://time.com/",
-        "urlToImage":"https://api.time.com/", "publishedAt":"2020-03-16T04:23:24Z" }
-      ]
-      }
-      ],
+      news_api: {"status":"ok","source":"time","sortBy":"top","articles":
+      [{"author":"Lissandra Villa","title":"Coronavirus Overshadows Debate as Joe Biden and Bernie Sanders Face Off","description":"Biden and Sanders sought to position themselves as the leader with the best vision to guide the country through a national emergency.","url":"http://time.com/5803702/democratic-debate-coronavirus/","urlToImage":"https://api.time.com/wp-content/uploads/2020/03/dem-debate-11-3.jpg?quality=85&crop=0px%2C28px%2C2400px%2C1256px&resize=1200%2C628&strip","publishedAt":"2020-03-16T04:23:24Z"},
+      {"author":"Shira Stein and Jennifer Jacobs / Bloomberg","title":"Key U.S. Health Agency Suffers Cyberattack During Coronavirus Response","description":"The U.S. Health and Human Services Department suffered a cyberattack Sunday night during the nation’s response to the coronavirus pandemic.","url":"http://time.com/5803816/coronavirus-cyberattack/","urlToImage":"https://api.time.com/wp-content/uploads/2020/03/coronavirus-cyberattack.jpg?quality=85&w=1024&h=628&crop=1","publishedAt":"2020-03-16T13:11:26Z"},
+      {"author":"Charlotte Alter","title":"Joe Biden Definitively Vows to Pick a Woman Vice President","description":"In the first one-on-one Democratic primary debate Sunday, former Vice President Joe Biden vowed to pick a woman Vice President.","url":"http://time.com/5803677/joe-biden-woman-vice-president/","urlToImage":"https://api.time.com/wp-content/uploads/2020/03/dem-debate-11-5.jpg?quality=85&w=1200&h=628&crop=1","publishedAt":"2020-03-16T01:47:31Z"},
+      {"author":"JOHN SEEWER / AP","title":"U.S. Hospitals Increasingly Worried About Surge in COVID-19 Cases","description":"Government and hospital leaders in the U.S. are increasingly worried about readiness for the new coronavirus outbreak.","url":"http://time.com/5803605/united-states-hospitals-coronavirus/","urlToImage":"https://api.time.com/wp-content/uploads/2020/03/united-states-coronavirus-hospitals.jpg?quality=85&w=1200&h=628&crop=1","publishedAt":"2020-03-15T21:52:27Z"},
+      {"author":"Jeffrey Kluger","title":"In the Wake of the Coronavirus, Here's Why Americans Are Hoarding Toilet Paper","description":"As America hunkers down against the novel coronavirus, there's one product that's in special demand: toilet paper.","url":"http://time.com/5803273/hoarding-toilet-paper/","urlToImage":"https://api.time.com/wp-content/uploads/2020/03/toiletpaper.jpg?quality=85&w=1200&h=628&crop=1","publishedAt":"2020-03-14T19:55:42Z"},
+      {"author":"Elijah Wolfson","title":"Mapping the Spread of the Coronavirus Outbreak Around the U.S. and the World","description":"See how coronavirus has spread all over the world.","url":"http://time.com/5800901/coronavirus-map/","urlToImage":"https://api.time.com/wp-content/uploads/2020/03/covid-map-march-11.png?w=1200&h=626&crop=1","publishedAt":"2020-03-11T17:01:48Z"},
+      {"author":"Hillary Leung / Hong Kong","title":"Why Wearing a Face Mask Is Encouraged in Asia, but Shunned in the U.S.","description":"As coronavirus continues to spread, face masks in public are common in many parts of Asia, but the U.S. discourages them.","url":"http://time.com/5799964/coronavirus-face-mask-asia-us/","urlToImage":"https://api.time.com/wp-content/uploads/2020/03/new-york-grand-central-mask.jpg?quality=85&w=1024&h=628&crop=1","publishedAt":"2020-03-12T08:05:45Z"}
+      ]},
 
             myArticles: [{"author":"Lissandra Villa","title":"Coronavirus Overshadows Debate as Joe Biden and Bernie Sanders Face Off","description":"Biden and Sanders sought to position themselves as the leader with the best vision to guide the country through a national emergency.","url":"http://time.com/5803702/democratic-debate-coronavirus/","urlToImage":"https://api.time.com/wp-content/uploads/2020/03/dem-debate-11-3.jpg?quality=85&crop=0px%2C28px%2C2400px%2C1256px&resize=1200%2C628&strip","publishedAt":"2020-03-16T04:23:24Z"},{"author":"Shira Stein and Jennifer Jacobs / Bloomberg","title":"Key U.S. Health Agency Suffers Cyberattack During Coronavirus Response","description":"The U.S. Health and Human Services Department suffered a cyberattack Sunday night during the nation’s response to the coronavirus pandemic.","url":"http://time.com/5803816/coronavirus-cyberattack/","urlToImage":"https://api.time.com/wp-content/uploads/2020/03/coronavirus-cyberattack.jpg?quality=85&w=1024&h=628&crop=1","publishedAt":"2020-03-16T13:11:26Z"},{"author":"Charlotte Alter","title":"Joe Biden Definitively Vows to Pick a Woman Vice President","description":"In the first one-on-one Democratic primary debate Sunday, former Vice President Joe Biden vowed to pick a woman Vice President.","url":"http://time.com/5803677/joe-biden-woman-vice-president/","urlToImage":"https://api.time.com/wp-content/uploads/2020/03/dem-debate-11-5.jpg?quality=85&w=1200&h=628&crop=1","publishedAt":"2020-03-16T01:47:31Z"},{"author":"JOHN SEEWER / AP","title":"U.S. Hospitals Increasingly Worried About Surge in COVID-19 Cases","description":"Government and hospital leaders in the U.S. are increasingly worried about readiness for the new coronavirus outbreak.","url":"http://time.com/5803605/united-states-hospitals-coronavirus/","urlToImage":"https://api.time.com/wp-content/uploads/2020/03/united-states-coronavirus-hospitals.jpg?quality=85&w=1200&h=628&crop=1","publishedAt":"2020-03-15T21:52:27Z"},{"author":"Jeffrey Kluger","title":"In the Wake of the Coronavirus, Here's Why Americans Are Hoarding Toilet Paper","description":"As America hunkers down against the novel coronavirus, there's one product that's in special demand: toilet paper.","url":"http://time.com/5803273/hoarding-toilet-paper/","urlToImage":"https://api.time.com/wp-content/uploads/2020/03/toiletpaper.jpg?quality=85&w=1200&h=628&crop=1","publishedAt":"2020-03-14T19:55:42Z"},{"author":"Elijah Wolfson","title":"Mapping the Spread of the Coronavirus Outbreak Around the U.S. and the World","description":"See how coronavirus has spread all over the world.","url":"http://time.com/5800901/coronavirus-map/","urlToImage":"https://api.time.com/wp-content/uploads/2020/03/covid-map-march-11.png?w=1200&h=626&crop=1","publishedAt":"2020-03-11T17:01:48Z"},{"author":"Hillary Leung / Hong Kong","title":"Why Wearing a Face Mask Is Encouraged in Asia, but Shunned in the U.S.","description":"As coronavirus continues to spread, face masks in public are common in many parts of Asia, but the U.S. discourages them.","url":"http://time.com/5799964/coronavirus-face-mask-asia-us/","urlToImage":"https://api.time.com/wp-content/uploads/2020/03/new-york-grand-central-mask.jpg?quality=85&w=1024&h=628&crop=1","publishedAt":"2020-03-12T08:05:45Z"}]
       // {
@@ -35,18 +37,19 @@ class App extends React.Component {
     xhr.open(
       "GET",
       "https://jsonplaceholder.typicode.com/posts",
-    //  "https://newsapi.org/v1/articles?source=time&sortBy=latest&apiKey=1d825f6378f2460d8bcf7edc35915e0f",
+    // "https://newsapi.org/v1/articles?source=time&sortBy=latest&apiKey=1d825f6378f2460d8bcf7edc35915e0f",
       true
     );
     xhr.onload = function (e) {
       if (xhr.readyState === 4) {
         if (xhr.status === 200) {
+      
        this.setState({json_objs : JSON.parse(xhr.responseText)});
-       //  this.setState({news_api : JSON.parse(xhr.responseText)});
+      //   this.setState({news_api : JSON.parse(xhr.responseText)});
        //  this.setState({myArticles : JSON.parse(this.news_api)});
 
         console.log(this.state.json_objs);
-        debugger;
+       //  debugger;
       //    this.state.jsonString = JSON.stringify(this.state.json_objs);
        //   console.log(jsonString);
           status = true;
@@ -92,6 +95,8 @@ description: {item.description}
   render() {
     console.log("rendering");
     console.log(this.state.news_api);
+  //  this.setState({myArticles : this.state.news_api.articles });
+
     return (
       <div>
 
